fd65475119425a2280e74b9ff54feb9720344032
JelleHille <74402442+JelleHille@users.noreply.github.com>
2021-01-14 10:29:37 +0100

    Update Button Exit Steam.py
    Knop: Moet op pin 23 worden aangesloten. En 3.3v power.

--- Button Exit Steam.py
+++ Button Exit Steam.py
@@ -3,6 +3,7 @@ import time
 GPIO.setmode( GPIO.BCM )
 GPIO.setwarnings( 0 )
 
+#Knop: Moet op pin 23 worden aangesloten. En 3.3v power.
 #Needs to always be running
 switch = 23
 GPIO.setup( switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN )
